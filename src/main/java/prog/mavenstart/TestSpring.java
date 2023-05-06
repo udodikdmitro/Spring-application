0b94d79357cd9d4b948834f0dcb261b57837e83d
dima udodik <dima udodik>
2023-05-06 10:24:37 +0300

    Init, destroy and factory methods.

--- src/main/java/prog/mavenstart/TestSpring.java
+++ src/main/java/prog/mavenstart/TestSpring.java
@@ -6,12 +6,29 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context
                 = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        firstMusicPlayer.playMusic();
+//
+//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//        System.out.println(comparison);
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(10);
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
+//
+//        System.out.println(firstMusicPlayer.getName());
+//        System.out.println(firstMusicPlayer.getVolume());
+
         context.close();
     }
 }
