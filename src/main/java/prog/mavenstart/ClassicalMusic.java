2d5f76b4337e598f3c421bd85098afc7d14263e7
dima udodik <dima udodik>
2023-05-06 14:33:59 +0300

    Component and autowired annotations.

--- src/main/java/prog/mavenstart/ClassicalMusic.java
+++ src/main/java/prog/mavenstart/ClassicalMusic.java
@@ -1,19 +1,9 @@
 package prog.mavenstart;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClassicalMusic implements Music{
-    private ClassicalMusic(){}
-
-    public static ClassicalMusic getClassicalMusic(){
-        return new ClassicalMusic();
-    }
-
-    public void doMyInit(){
-        System.out.println("doing my initialization");
-    }
-
-    public void doMyDestroy(){
-        System.out.println("Doing my destruction");
-    }
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- src/main/java/prog/mavenstart/TestSpring.java
+++ src/main/java/prog/mavenstart/TestSpring.java
@@ -7,29 +7,20 @@ public class TestSpring {
         ClassPathXmlApplicationContext context
                 = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
-        System.out.println(classicalMusic.getSong());
+//        Music music = context.getBean("rockMusic", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        musicPlayer.playMusic();
+//
+//        Music music2 = context.getBean("classicalMusic", Music.class);
+//        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
+//        classicalMusicPlayer.playMusic();
 
-        //Music music = context.getBean("musicBean", Music.class);
-        //MusicPlayer musicPlayer = new MusicPlayer(music);
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
 
-//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        firstMusicPlayer.playMusic();
-//
-//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-//        System.out.println(comparison);
-//        System.out.println(firstMusicPlayer);
-//        System.out.println(secondMusicPlayer);
-//
-//        firstMusicPlayer.setVolume(10);
-//        System.out.println(firstMusicPlayer.getVolume());
-//        System.out.println(secondMusicPlayer.getVolume());
-//
-//        System.out.println(firstMusicPlayer.getName());
-//        System.out.println(firstMusicPlayer.getVolume());
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
 }
-
